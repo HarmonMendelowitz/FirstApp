5945265a6dcd51496bad87e5a45921b2ca530f45
HarmonMendelowitz <mendelowitz@gwu.edu>
2019-01-22 19:55:53 -0500

    kotlin

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -8,5 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val string = "hello world"
+        //val cannot be reassigned
+        var string2 : String? = "created var"
+        string2 = "new stuff"
+        string2 = null
+
+
+    }
+
+    private fun getStringLength(s : String?): Int{
+        return s?.length ?:-1
     }
 }
